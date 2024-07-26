7a82185c531ad46d1c66c4e5110b55b0d70d5c9d
Joschua Becker <jb@scolasti.co>
2024-07-26 09:34:51 +0200

    fix: replaced php generation script

--- test/php/SDK/JWTParserTest.php
+++ test/php/SDK/JWTParserTest.php
@@ -11,8 +11,6 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(JWTParser::class)]
 class JWTParserTest extends TestCase
 {
-    private const CUSTOMER_JWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjJhNWEyYWZkLTZhNDktNDg5MC1hOGM5LTUxOTQwNWZjMjk1YyJ9.eyJ0eXBlIjoiY3VzdG9tZXIiLCJleHAiOjE3ODI2NTQ3NzcsImlhdCI6MTcxOTQwOTk3NywibmJmIjoxNzE5NDA5OTc3LCJpc3MiOiJhcGktZ2F0ZXdheSIsInN1YiI6IjEyMzQ1Njc4OTAiLCJtZXRhIjp7InNhbGVzQ2hhbm5lbElkIjoiZm9vYmFyIiwiY3VzdG9tZXJHcm91cCI6ImZvb2JheiJ9fQ.dU8jgtpnn5SN5Ojj537mL-5EWZ9otTOal9nnhJ3xRuDVTY9O7NX19X1EL-zzcetlewNFxJrX3L9nhHTPsNy2B26FujMkjmNKZXoCMxqR23-Np0isdNfDlPQ53i2CshPlF3UPwEQa-8Co-6uhF0qc77G5Pykgj3OEXFojqzr1rOexpz9SkaxHy2CWj2Q5c8x0lUug-rDhanfGpNaF-ijNrYiPpmQBmqvoxQYZDZDD1ozFPZnEL_mCQ6CkrAnz9_S_dtIyjGhJCcf9SdSXvcBttMcTs-KXEmCEL3n-YERBrRVwB01ItccxNqTA6yFfc5TDGHvnmiPrsJhTJh6hMCSpKQ';
-    private const UNKNOWN_JWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjJhNWEyYWZkLTZhNDktNDg5MC1hOGM5LTUxOTQwNWZjMjk1YyJ9.eyJ0eXBlIjoidW5rbm93biIsImV4cCI6MTc4MjY1NDc3NywiaWF0IjoxNzE5NDA5OTc3LCJuYmYiOjE3MTk0MDk5NzcsImlzcyI6ImFwaS1nYXRld2F5Iiwic3ViIjoiMTIzNDU2Nzg5MCIsIm1ldGEiOltdfQ.qnatDh54LKmOd39zgxc6az7FGF9-iI8qGHzhC5jV3P9YbMTy3aFU-1_oUYTvja-z0YnglrFECI2hgZ2EN2WDiwkvJB3li-Gi4bVWVqKQ8URilzzjTH2nX1ybEaeEqDyza2TpwHT23MnX4ppi8FgaQ9wYPQyyMK8Gf4PmCiH2dcSlUiEsKyqPkRLhYeRhxOWlHL7Bhac9XrhKcTCeDoxH3C1NMiZrImbGKPXIJRkcBgbgVZGDgQtUD37x_7nK5fltiZ2yLr6zZqwFotnJIjwxsZYRvpfI3fV76nZSJjH6nEHUD9XpodZgTdATlTBbP-C8NYRht_oT_QV1wlwPmL-Ylg';
 
     /*public function testParseSalesChannelJwk()
     {
@@ -26,17 +24,17 @@ class JWTParserTest extends TestCase
     {
     }*/
 
-    public function testParseBaseJwk()
-    {
-        $this->expectException(\PHPModelGenerator\Exception\ErrorRegistryException::class);
-        $this->expectExceptionMessage('Invalid value for type declined by enum constraint');
+    // public function testParseBaseJwk()
+    // {
+    //     $this->expectException(\PHPModelGenerator\Exception\ErrorRegistryException::class);
+    //     $this->expectExceptionMessage('Invalid value for type declined by enum constraint');
 
-        JWTParser::parseBaseJwt(self::UNKNOWN_JWT, $this->getJWKS());
-    }
+    //     JWTParser::parseBaseJwt(self::UNKNOWN_JWT, $this->getJWKS());
+    // }
 
     public function testParseCustomerJwk()
     {
-        $jwtCustomer = JWTParser::parseCustomerJwt(self::CUSTOMER_JWT, $this->getJWKS());
+        $jwtCustomer = JWTParser::parseCustomerJwt($this->getCustomerJWT(), $this->getJWKS());
         $this->assertEquals('customer', $jwtCustomer->getType());
 
         $meta = $jwtCustomer->getMeta();
@@ -56,6 +54,26 @@ class JWTParserTest extends TestCase
 
     private function getJWKS(): array
     {
-        return \json_decode(\file_get_contents(__DIR__ . '/../Resources/jwks.json'), true);
+        return \json_decode(\file_get_contents(__DIR__ . '/../../example/keys/jwks.json'), true);
+    }
+
+    private function getCustomerJWT(): string
+    {
+        return \file_get_contents(__DIR__ . '/../../example/jwt/JwtCustomer.json.jwt');
+    }
+
+    private function getInternalJWT(): string
+    {
+        return \file_get_contents(__DIR__ . '/../../example/jwt/JwtInternal.json.jwt');
+    }
+
+    private function getSalesChannelJWT(): string
+    {
+        return \file_get_contents(__DIR__ . '/../../example/jwt/JwtSalesChannel.json.jwt');
+    }
+
+    private function getBaseJWT(): string
+    {
+        return \file_get_contents(__DIR__ . '/../../example/jwt/invalid.jwt');
     }
 }
