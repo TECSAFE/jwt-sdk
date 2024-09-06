3268bc3b5fac9eb2da4dc24e56b685c8ff140732
Joschua Becker <jb@scolasti.co>
2024-09-06 07:11:54 +0200

    fix: jest implementation with new build pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'], // Treat TypeScript files as ESM
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { // Use ts-jest for TypeScript files with inlined configuration
+      useESM: true, // Ensures that ts-jest knows to handle ESM syntax
+    }],
+  },
+  moduleNameMapper: {
+    // Map .js extensions back to .ts for Jest
+    '^(.*)\\.js$': '$1',
+  },
 };
